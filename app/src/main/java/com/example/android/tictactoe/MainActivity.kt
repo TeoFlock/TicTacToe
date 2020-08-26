16fc4476b99c7a033380a85a51c42df2d5aca70a
teoflock <matteoflock@gmail.com>
2020-08-26 18:08:12 +0200

    Git repository created

--- app/src/main/java/com/example/android/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/android/tictactoe/MainActivity.kt
@@ -5,30 +5,12 @@ import android.os.Bundle
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val fragmentManager = supportFragmentManager
 
-        val buttonAbout = findViewById<Button>(R.id.button_about)
-        buttonAbout.setOnClickListener {
-            val fragmentTransaction = fragmentManager.beginTransaction()
-
-            fragmentTransaction.replace(R.id.activity_main_layout, AboutFragment())
-            fragmentTransaction.setReorderingAllowed(true)
-            fragmentTransaction.addToBackStack(null)
-            fragmentTransaction.commit()
-        }
-
-        val buttonPlay = findViewById<Button>(R.id.button_play)
-        buttonPlay.setOnClickListener {
-            val fragmentTransaction = fragmentManager.beginTransaction()
-
-            fragmentTransaction.replace(R.id.activity_main_layout, PlayGroundFragment())
-            fragmentTransaction.addToBackStack(null)
-            fragmentTransaction.commit()
-        }
     }
 }
 
